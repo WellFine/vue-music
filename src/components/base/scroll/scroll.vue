4be5db3e2ee0efe785fe710ac1d6f64cfb7682f6
unknown <1339531047@qq.com>
2021-11-14 00:30:19 +0800

    feat: 新增 BetterScroll 滚动组件

---
+++ src/components/base/scroll/scroll.vue
@@ -0,0 +1,35 @@
+<template>
+  <div ref="rootRef">
+    <!-- BetterScroll 只针对第一个子元素生效，所以套一层 div -->
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { ref } from 'vue'
+  import useScroll from './useScroll'
+
+  export default {
+    name: 'Scroll',
+    props: {
+      click: {
+        type: Boolean,
+        default: true
+      }
+    },
+    setup (props) {
+      const rootRef = ref(null)
+      useScroll(rootRef, props)
+
+      return {
+        rootRef
+      }
+    }
+  }
+</script>
+
+<style lang='scss' scoped>
+
+</style>
