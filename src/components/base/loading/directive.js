fecb1b9d0421af54efd83794ccd1294ececa7348
unknown <1339531047@qq.com>
2021-11-14 21:59:49 +0800

    feat: 自定义 v-loading 全局指令

---
+++ src/components/base/loading/directive.js
@@ -0,0 +1,78 @@
+/**
+ * 该指令将 loading 组件生成的 DOM 动态插入到指令作用的 DOM 对象上
+ */
+import { createApp } from 'vue'
+import { addClass, removeClass } from '@/assets/js/dom'
+import Loading from './loading.vue'
+
+const relativeClass = 'g-relative'
+
+const loadingDirective = {
+  // 在指令绑定元素的父组件被挂载后调用
+  mounted (el, binding) {
+    /**
+     * 创建一个新的 vue 实例，其根组件就是 loading 组件
+     * vue 并不是只能在 main.js 中创建一个 vue 实例
+     * main.js 中的 vue 实例，其根组件是 App.vue
+     * 开发过程中可以灵活的创建新的 vue 实例，然后使用实例的 mount 方法挂载实例组件
+     */
+    // app 是 loading 组件应用实例
+    const app = createApp(Loading)
+    // instance 是 loading 根组件实例，是将 loading 组件挂载到新创建的 div 后返回
+    const instance = app.mount(document.createElement('div'))
+
+    // 将 instance 实例存下来，方便 updated 函数中使用
+    el.instance = instance
+
+    setLoadingInfo(instance, binding.arg)
+
+    // v-loading=true
+    if (binding.value) {
+      append(el)
+    }
+  },
+
+  // 在包含组件的 VNode 及其子组件的 VNode 更新后调用
+  updated (el, binding) {
+    setLoadingInfo(el.instance, binding.arg)
+
+    // 如果 v-loading 指令的值改变
+    if (binding.value !== binding.oldValue) {
+      binding.value ? append(el) : remove(el)
+    }
+  }
+}
+
+function append (el) {
+  // 如果指令作用的 DOM 没有定位，则加上定位以便 loading 的 absolute 生效
+  // loading 使用 fixed 则不需要这段逻辑
+  // setRelative(el)
+
+  // instance 是 loading 根组件实例，instance.$el 就是 loading 组件对应的 DOM 对象
+  el.appendChild(el.instance.$el)
+}
+
+function remove (el) {
+  removeClass(el, relativeClass)
+  el.removeChild(el.instance.$el)
+}
+
+// eslint-disable-next-line no-unused-vars
+function setRelative (el) {
+  // Window.getComputedStyle() 方法返回 CSS 样式对象
+  const style = getComputedStyle(el)
+  // 如果指令作用的 DOM 没有定位，则加上定位以便 loading 的 absolute 生效
+  if (['fixed', 'relative', 'absolute'].indexOf(style.position) === -1) {
+    // g-relative 这个类已经在 @/assets/scss/base.scss 中定义了，作用就是加上 position: relative; 这个样式
+    addClass(el, relativeClass)
+  }
+}
+
+function setLoadingInfo (instance, info) {
+  // 如果 info 即 binding.arg 存在, 则通过 loading 实例 instance 调用 loading 的 setInfo method 设置 info data
+  if (typeof info !== 'undefined') {
+    instance.setInfo(info)
+  }
+}
+
+export default loadingDirective
--- src/main.js
+++ src/main.js
@@ -7,10 +7,16 @@ import lazyPlugin from 'vue3-lazy'
 // 引入全局实体样式文件
 import '@/assets/scss/index.scss'
 
+// v-loading 指令
+import loadingDirective from '@/components/base/loading/directive'
+
 createApp(App).use(store).use(router).use(lazyPlugin, {
   /**
    * require 是 webpack 可以识别的语法，可以找到对应的图片，然后使用相应的 loader 来处理
    * 最终将图片处理为 base64 或把图片变为外链，然后通过外链加载图片
    */
   loading: require('@/assets/images/default.png')
-}).mount('#app')
+})
+// app.directive 全局注册指令，但要注意，在这个 app 下注册的指令，只能这个 app 下的组件可以使用
+.directive('loading', loadingDirective)
+.mount('#app')
