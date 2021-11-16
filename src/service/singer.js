55db5129b36615015360a0141e2cd2d520ab5ed7
unknown <1339531047@qq.com>
2021-11-16 23:03:33 +0800

    feat: 获取歌手不包含播放 url 的歌曲列表

--- src/service/singer.js
+++ src/service/singer.js
@@ -3,3 +3,10 @@ import { get } from './base'
 export function getSingerList () {
   return get('/api/getSingerList')
 }
+
+// 获取歌手详情，即歌手的歌曲列表，但歌曲列表不包含歌曲播放 url，需通过 ./song.js 的 processSongs 获取
+export function getSingerDetail (singerMid) {
+  return get('/api/getSingerDetail', {
+    mid: singerMid
+  })
+}
