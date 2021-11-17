1bd415ed6b0de9f9c4890f55c0fb3a45bac73408
unknown <1339531047@qq.com>
2021-11-18 00:36:48 +0800

    feat: 播放器相关 state

---
+++ src/store/state.js
@@ -0,0 +1,12 @@
+import { PLAY_MODE } from '@/assets/js/constant'
+
+const state = {
+  sequenceList: [], // 顺序的播放列表，作为歌曲列表的原始数据
+  playlist: [], // 真实的播放列表，随着播放模式不同而改变原始顺序列表得来
+  playing: false, // 播放状态
+  playMode: PLAY_MODE.sequence, // 播放模式，顺序，循环，随机，默认顺序
+  currentIndex: 0, // 当前播放歌曲索引
+  fullScreen: false // 播放器的状态，是全屏还是收缩，默认收缩
+}
+
+export default state
