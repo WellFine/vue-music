31bc038bb69f0e73362037398397ffb70a075692
unknown <1339531047@qq.com>
2021-11-18 00:38:50 +0800

    feat: 播放器相关状态管理

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
-import { createStore } from 'vuex'
+import { createStore, createLogger } from 'vuex'
+import state from './state'
+import * as getters from './getters'
+import mutations from './mutations'
+import * as actions from './actions'
+
+const debug = process.env.NODE_ENV !== 'production'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state,
+  getters,
+  mutations,
+  actions,
+  strict: debug, // 开发环境下开启严格模式
+  plugins: debug ? [createLogger()] : [] // 开发环境下使用 createLogger 插件查看提交状态
 })
