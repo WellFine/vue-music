95c902c4098d4525aae0358d2201f3399ccf5aa6
unknown <1339531047@qq.com>
2021-11-18 23:45:49 +0800

    feat: 全局播放器组件

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <com-header />
   <com-tab />
   <router-view />
+  <com-player />
 </template>
 
 <script>
   import Header from '@/components/header/header'
   import Tab from '@/components/tab/tab'
+  import Player from '@/components/player/player'
 
   export default {
     components: {
       ComHeader: Header,
-      ComTab: Tab
+      ComTab: Tab,
+      ComPlayer: Player
     }
   }
 </script>
