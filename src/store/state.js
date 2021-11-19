95ab094e37a8666e786fc4e2e293e1db2d5ab69f
unknown <1339531047@qq.com>
2021-11-19 22:59:45 +0800

    feat: 收藏歌曲 state

--- src/store/state.js
+++ src/store/state.js
@@ -1,4 +1,5 @@
-import { PLAY_MODE } from '@/assets/js/constant'
+import { PLAY_MODE, FAVORITE_KEY } from '@/assets/js/constant'
+import { load } from '@/assets/js/array-store'
 
 const state = {
   sequenceList: [], // 顺序的播放列表，作为歌曲列表的原始数据
@@ -6,7 +7,8 @@ const state = {
   playing: false, // 播放状态
   playMode: PLAY_MODE.sequence, // 播放模式，顺序，循环，随机，默认顺序
   currentIndex: 0, // 当前播放歌曲索引
-  fullScreen: false // 播放器的状态，是全屏还是收缩，默认收缩
+  fullScreen: false, // 播放器的状态，是全屏还是收缩，默认收缩
+  favoriteList: load(FAVORITE_KEY) // 歌曲收藏列表，与本地缓存相结合使用
 }
 
 export default state
