681ebac6fedadd1eb80e12ead3d309024ce1b5fb
unknown <1339531047@qq.com>
2021-11-19 23:01:49 +0800

    feat: 切换播放模式 action

--- src/store/actions.js
+++ src/store/actions.js
@@ -19,3 +19,23 @@ export function randomPlay ({ commit }, list) {
   commit('setFullScreen', true) // 展开播放器
   commit('setCurrentIndex', 0) // 播放随机列表第一首歌
 }
+
+export function changeMode ({ state, getters, commit }, mode) {
+  const currentSongId = getters.currentSong.id
+
+  if (mode === PLAY_MODE.random) {
+    // 如果切换为随机播放，则将原始顺序播放列表打乱
+    commit('setPlaylist', shuffle(state.sequenceList))
+  } else {
+    // 如果切换为顺序和循环播放，则播放原始播放列表
+    commit('setPlaylist', state.sequenceList)
+  }
+
+  /**
+   * 随机播放把列表打乱后，currentSong 当前播放歌曲因为 getters 中列表变化而索引没变，会导致 currentSong 大概率会变化
+   * 在打乱后的列表中找到正在播放的歌曲下标，然后重新设置 currentIndex
+   */
+  const index = state.playlist.findIndex(song => song.id === currentSongId)
+  commit('setCurrentIndex', index)
+  commit('setPlayMode', mode)
+}
