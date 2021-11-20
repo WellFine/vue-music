e58b259a2f361ffa80c88d8c54ed563471588c61
unknown <1339531047@qq.com>
2021-11-21 00:15:37 +0800

    feat: 获取歌词接口

--- src/service/song.js
+++ src/service/song.js
@@ -19,3 +19,25 @@ export function processSongs (songs) {
     }).filter(song => song.url.includes('vkey')) // 如果歌曲 url 是有效的，那么 url 里面一定含有 vkey 这个字符串，如果没有则是无效 url，需要排除
   })
 }
+
+// 不同列表下的歌曲可能 mid 相同，mid 相同的歌曲是同一首歌，歌词也一样，那么使用映射来存储已经获取过的歌词，下次请求时查看映射中是否请求过当前 mid 的歌词
+const lyricMap = {}
+
+export function getLyric (song) {
+  // 如果歌曲本身已经有歌词了，直接返回 resolved 状态的 promise
+  if (song.lyric) return Promise.resolve(song.lyric)
+
+  const mid = song.mid
+
+  // 如果映射中存在当前 mid 的歌词，直接返回
+  const lyric = lyricMap[mid]
+  if (lyric) return Promise.resolve(lyric)
+
+  return get('/api/getLyric', {
+    mid
+  }).then(res => {
+    const lyric = res ? res.lyric : '[00:00:00]该歌曲暂时无法获取歌词'
+    lyricMap[mid] = lyric
+    return lyric
+  })
+}
