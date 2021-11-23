c26a2015a8010597926aef8cba6ff4da9d774f07
unknown <1339531047@qq.com>
2021-11-23 22:20:20 +0800

    feat: 一级路由视图组件高度自适应

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <com-header />
   <com-tab />
-  <router-view />
+  <router-view :style="viewStyle" />
   <com-player />
 </template>
 
 <script>
+  import { mapState } from 'vuex'
+
   import Header from '@/components/header/header'
   import Tab from '@/components/tab/tab'
   import Player from '@/components/player/player'
@@ -15,6 +17,16 @@
       ComHeader: Header,
       ComTab: Tab,
       ComPlayer: Player
+    },
+    computed: {
+      ...mapState(['playlist']),
+      viewStyle () {
+        // 当 mini-player.vue 出现时，将一级路由对应的页面向上移 mini-player.vue 的高度
+        const bottom = this.playlist.length ? '60px' : '0px'
+        return {
+          bottom
+        }
+      }
     }
   }
 </script>
