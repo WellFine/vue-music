e2bbec943ff791eb642fb2a6fdff94b39c940639
unknown <1339531047@qq.com>
2021-11-24 23:41:59 +0800

    refactor: 改变函数参数

--- src/service/singer.js
+++ src/service/singer.js
@@ -5,8 +5,8 @@ export function getSingerList () {
 }
 
 // 获取歌手详情，即歌手的歌曲列表，但歌曲列表不包含歌曲播放 url，需通过 ./song.js 的 processSongs 获取
-export function getSingerDetail (singerMid) {
+export function getSingerDetail (singer) {
   return get('/api/getSingerDetail', {
-    mid: singerMid
+    mid: singer.mid
   })
 }
