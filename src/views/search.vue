85adf5410260ce79cfa21472ed970dda73394b5f
unknown <1339531047@qq.com>
2021-11-28 21:35:58 +0800

    feat: 搜索框与热门搜索关键词

--- src/views/search.vue
+++ src/views/search.vue
@@ -1,13 +1,96 @@
 <template>
-  <div>搜索页面</div>
+  <div class="search">
+    <div class="search-input-wrapper">
+      <!--
+        search-input 内部提供 query 供外部使用，debounce 等操作都由 search-input 内部完成
+        外部使用时只需要监听 query 变化即可
+      -->
+      <search-input v-model="query"></search-input>
+    </div>
+    <div class="search-content">
+      <div class="hot-keys">
+        <h1 class="title">热门搜索</h1>
+        <ul>
+          <li
+            class="item"
+            v-for="key in hotKeys" :key="key.id"
+            @click="addQuery(key.key)"
+          >
+            <span>{{ key.key }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'search'
-}
+  import { ref } from 'vue'
+  import SearchInput from '@/components/search/search-input'
+  import { getHotKeys } from '@/service/search'
+
+  export default {
+    name: 'search',
+    components: {
+      SearchInput
+    },
+    setup () {
+      const query = ref('')
+      const hotKeys = ref([])
+
+      getHotKeys().then(res => {
+        hotKeys.value = res.hotKeys
+      })
+
+      const addQuery = key => {
+        query.value = key
+      }
+
+      return {
+        query,
+        hotKeys,
+        addQuery
+      }
+    }
+  }
 </script>
 
 <style lang='scss' scoped>
+  .search {
+    position: fixed;
+    width: 100%;
+    top: 88px;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
 
+    .search-input-wrapper {
+      margin: 10px 20px 20px;
+    }
+
+    .search-content {
+      flex: 1;
+      overflow: hidden;
+
+      .hot-keys {
+        margin: 0 20px 20px;
+
+        .title {
+          margin-bottom: 20px;
+          font-size: $font-size-medium;
+          color: $color-text-l;
+        }
+
+        .item {
+          display: inline-block;
+          padding: 5px 10px;
+          margin: 0 20px 10px 0;
+          border-radius: 6px;
+          background: $color-highlight-background;
+          font-size: $font-size-medium;
+          color: $color-text-d;
+        }
+      }
+    }
+  }
 </style>
